9987bbda64535407cce45ae580e812628c339762
vincentdaogithub <vincentdaomain@gmail.com>
2022-10-20 14:18:31 +0700

    major edit

--- List/src/main/javadoc/apidocs/type-search-index.js
+++ List/src/main/javadoc/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.vincentdao.List","l":"App"},{"p":"com.vincentdao.List","l":"IQueue"},{"p":"com.vincentdao.List","l":"IStack"},{"p":"com.vincentdao.List","l":"LinkedList"},{"p":"com.vincentdao.List","l":"List"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.vincentdao.List","l":"IQueue"},{"p":"com.vincentdao.List","l":"IStack"},{"p":"com.vincentdao.List","l":"LinkedList"},{"p":"com.vincentdao.List","l":"List"}];updateSearchResults();
